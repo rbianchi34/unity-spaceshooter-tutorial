44cec599ff6952ca8c2fa1043d978cd556c867ae
Rocio <rbianchi34@gmail.com>
2017-11-22 18:28:15 -0300

    .

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject[] hazards;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public int enemyShipAppear;
+	public float spawnWave;
+	public float startWait;
+	public float waveWait;
+
+	public Text scoreText;
+	public Text restartText;
+	public Text gameOverText;
+	public Text waveCountText;
+	private int score;
+	private bool gameOver;
+	private bool restart;
+	private int waveCount;
+	public float waveTextDisable;
+
+
+	void Start(){
+		Cursor.visible = false;
+		waveCount=1;
+		StartCoroutine(SpawnWaves());
+		StartCoroutine(UpdateWaveCount());
+		score = 0;
+		UpdateScore();
+		gameOver=false;
+		restart=false;
+		restartText.text="";
+		gameOverText.text="";
+	}
+
+	void Update(){
+		if(restart){
+			if(Input.GetKeyDown(KeyCode.R)){
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+			}
+		}
+	}
+
+	IEnumerator SpawnWaves(){
+
+		yield return new WaitForSeconds(startWait);
+
+		while(true){
+			for(int i = 0 ; i<hazardCount ; i++)
+			{	
+				GameObject hazard = hazards [Random.Range(0,2)];
+				if(waveCount>=enemyShipAppear){
+					hazard = hazards [Random.Range(1,3)];
+				}
+				if(waveCount>=enemyShipAppear*2){
+					hazard = hazards [Random.Range(0,hazards.Length)];
+				}
+				
+				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate(hazard,spawnPosition,spawnRotation);
+				yield return new WaitForSeconds(spawnWave);	
+				
+			}
+
+			yield return new WaitForSeconds(waveWait);
+			waveCount++;
+			
+			StartCoroutine(UpdateWaveCount());
+
+			if(gameOver){
+				restartText.text="Press 'R' to Restart";
+				restart=true;
+				break;
+			}
+		}
+	}
+
+	void UpdateScore(){
+		scoreText.text="Score: " + score;
+	}
+
+	public void AddScore (int newScoreValue){
+		score += newScoreValue;
+		UpdateScore();
+	}
+
+	public void GameOver(){
+		gameOverText.text="Game Over";
+		gameOver=true;
+	}
+
+	IEnumerator UpdateWaveCount(){
+		if(gameOver==false){
+		waveCountText.text="Wave "+waveCount;
+		yield return new WaitForSeconds(waveTextDisable);
+		waveCountText.text="";
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax,zMin,zMax;
+}
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public float tilt;
+	public float speed;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire = 0.0F;
+	private AudioSource shotAudio;
+
+			
+
+
+	void Start(){
+
+		rb = GetComponent<Rigidbody>();
+		shotAudio = GetComponent<AudioSource>();
+	}
+
+	void FixedUpdate(){
+
+	//MOVEMENT
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal * speed,0,moveVertical * speed);
+
+		rb.AddForce(movement);
+		rb.position=new Vector3
+		(
+			Mathf.Clamp(rb.position.x,boundary.xMin,boundary.xMax), //boundary whitin two values, which get compared to the rb value
+			0.0f, // Y. No give up and down.
+			Mathf.Clamp(rb.position.z,boundary.zMin,boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0,0,rb.velocity.x * -tilt);
+	}
+
+	void Update(){
+	//SHOOTING
+		if((Input.GetButton("Fire1")||Input.GetKeyDown(KeyCode.Space))&&Time.time>nextFire){
+			nextFire = Time.time + fireRate;
+			GameObject clone = Instantiate(shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+			shotAudio.Play();
+		}
+		
+	}
+}
